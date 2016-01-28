6cd4b8e1b93894098e73395cb566a0b527dc86c1
Daniel Prince <programmer96@hotmail.com>
2016-01-28 12:58:30 -0400

    Activity to display positive logs

--- app/src/main/java/com/forzipporah/mylove/ui/PositiveLogActivity.java
+++ app/src/main/java/com/forzipporah/mylove/ui/PositiveLogActivity.java
@@ -2,6 +2,9 @@ package com.forzipporah.mylove.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.forzipporah.mylove.R;
 
@@ -13,4 +16,21 @@ public class PositiveLogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_positive_log);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_positvelog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_create:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
