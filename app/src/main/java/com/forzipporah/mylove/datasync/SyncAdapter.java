5b4677a9fb08c7e2b877b9e85f40dc82077fc736
Daniel Prince <programmer96@hotmail.com>
2016-02-16 16:49:25 -0400

    able to sync more data

--- app/src/main/java/com/forzipporah/mylove/datasync/SyncAdapter.java
+++ app/src/main/java/com/forzipporah/mylove/datasync/SyncAdapter.java
@@ -10,11 +10,17 @@ import android.content.SyncResult;
 import android.os.Bundle;
 
 import com.forzipporah.mylove.database.contracts.ILoveContract;
+import com.forzipporah.mylove.database.contracts.MemoryContract;
 import com.forzipporah.mylove.database.contracts.PoemContract;
+import com.forzipporah.mylove.database.contracts.PromiseContract;
+import com.forzipporah.mylove.database.contracts.ReassureContract;
 import com.forzipporah.mylove.helpers.Notification;
 import com.forzipporah.mylove.helpers.http.HttpRequestManager;
 import com.forzipporah.mylove.models.ILove;
+import com.forzipporah.mylove.models.Memory;
 import com.forzipporah.mylove.models.Poem;
+import com.forzipporah.mylove.models.Promise;
+import com.forzipporah.mylove.models.Reassure;
 
 import java.io.IOException;
 
@@ -44,30 +50,57 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         // define request objects with there destination to retrieve data
         HttpRequestManager httpRequestPoems      = new HttpRequestManager("poem.php", "fetch");
         HttpRequestManager httpRequestLoveAbouts = new HttpRequestManager("ilove.php", "fetch");
+        HttpRequestManager httpRequestPromise  = new HttpRequestManager("promise.php", "fetch");
+        HttpRequestManager httpRequestMemory   = new HttpRequestManager("memory.php", "fetch");
+        HttpRequestManager httpRequestReassure = new HttpRequestManager("reassure.php", "fetch");
 
-
-        // define strings to hold received data
-        String poemResponse  = null;
-        String iloveResponse = null;
+        // variables to hold data
+        String poemResponse = null,
+                iloveResponse = null,
+                promiseResponse = null,
+                reassureResponse = null,
+                memoryResponse = null;
+        // retrieve data
         try {
-            // fetch data from server
             poemResponse = httpRequestPoems.connect();
             iloveResponse = httpRequestLoveAbouts.connect();
+            promiseResponse = httpRequestPromise.connect();
+            memoryResponse = httpRequestMemory.connect();
+            reassureResponse = httpRequestReassure.connect();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // validate data and insert them
 
-        // Data for stuff I love
+        // poems
+        if (validateResponse(poemResponse)) {
+            ContentValues[] values = Poem.createContentValuesArrayFromJSON(poemResponse);
+            getContext().getContentResolver().bulkInsert(PoemContract.buildUri(), values);
+        }
+
+        // i loves
         if (validateResponse(iloveResponse)) {
             ContentValues[] values = ILove.createContentValuesArrayFromJSON(iloveResponse);
             getContext().getContentResolver().bulkInsert(ILoveContract.buildUri(), values);
         }
 
-        // Data for poems
-        if (validateResponse(poemResponse)) {
-            ContentValues[] values = Poem.createContentValuesArrayFromJSON(poemResponse);
-            getContext().getContentResolver().bulkInsert(PoemContract.buildUri(), values);
+        // promises
+        if (validateResponse(promiseResponse)) {
+            ContentValues[] values = Promise.createContentValuesArrayFromJSON(promiseResponse);
+            getContext().getContentResolver().bulkInsert(PromiseContract.buildUri(), values);
+        }
+
+        // memories
+        if (validateResponse(memoryResponse)) {
+            ContentValues[] values = Memory.createContentValuesArrayFromJSON(memoryResponse);
+            getContext().getContentResolver().bulkInsert(MemoryContract.buildUri(), values);
+        }
+
+        // reassurance
+        if (validateResponse(reassureResponse)) {
+            ContentValues[] values = Reassure.createContentValuesArrayFromJSON(reassureResponse);
+            getContext().getContentResolver().bulkInsert(ReassureContract.buildUri(), values);
         }
 
         // if data is received show notification
